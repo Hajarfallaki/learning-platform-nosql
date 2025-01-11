0380fe9eaff1adaf2d432c109471a5b62d844075
hajar_elfallaki_idrissi <hajarfallaki24@gmail.com>
2025-01-11 22:06:16 +0100

    Ajout de la fonction findOneById dans les services

--- src/services/mongoService.js
+++ src/services/mongoService.js
@@ -1,14 +1,24 @@
 // Question: Pourquoi créer des services séparés ?
-// Réponse: 
+
+// Réponse: Créer des services séparés permet d'encapsuler et de réutiliser la logique métier 
+// et les interactions avec la base de données, ce qui améliore la maintenabilité et la lisibilité 
+// du code. Cela permet également de centraliser les opérations communes comme les requêtes à la base de données, 
+// ce qui réduit la duplication de code dans les contrôleurs. En séparant ces préoccupations, chaque composant 
+// de l'application reste simple et concentre son rôle, facilitant ainsi les tests et les modifications futures.
 
 const { ObjectId } = require('mongodb');
 
 // Fonctions utilitaires pour MongoDB
 async function findOneById(collection, id) {
-  // TODO: Implémenter une fonction générique de recherche par ID
+  if (!ObjectId.isValid(id)) {
+    throw new Error('ID invalide');
+  }
+  const result = await db.collection(collection).findOne({ _id: ObjectId(id) });
+  return result;
 }
 
 // Export des services
 module.exports = {
-  // TODO: Exporter les fonctions utilitaires
+  findOneById,
+  
 };
